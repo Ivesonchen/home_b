a9420451b44d7e9c6692adfda823c738fd52cc48
Xiaoqian <ivesonchen@gmail.com>
2018-10-24 01:00:38 -0400

    finish request

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
-
+import requests
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'INDEX'
+    url = "https://raw.githubusercontent.com/Val-istar-Guo/article/master/articles/%E4%B8%BA%E8%84%8F%E6%95%B0%E6%8D%AE%E6%B6%88%E6%AF%92.md"
+    return requests.get(url).text
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug = True,port = 8080)
